fcb657723890bceeffccae6eabec6bb20a547f8f
Matthieu Muffato <muffato@ebi.ac.uk>
2017-05-01 16:35:00 +0100

    Standard script documentation

--- scripts/hive.pl
+++ scripts/hive.pl
@@ -76,3 +76,42 @@ if ($@) {
     $script_object->main();
 }
 
+
+__DATA__
+
+=pod
+
+=head1 NAME
+
+hive.pl <action> [options]
+
+=head1 DESCRIPTION
+
+hive.pl is a generic wrapper for the script module interface. It allows to run all the
+eHive commands as "hive init", "hive seed" etc.
+
+=head1 USAGE EXAMPLES
+
+
+=head1 OPTIONS
+
+=head1 LICENSE
+
+    Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+    Copyright [2016-2017] EMBL-European Bioinformatics Institute
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the License
+    is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and limitations under the License.
+
+=head1 CONTACT
+
+    Please subscribe to the Hive mailing list:  http://listserver.ebi.ac.uk/mailman/listinfo/ehive-users  to discuss Hive-related questions or to be notified of our updates
+
+=cut
+
